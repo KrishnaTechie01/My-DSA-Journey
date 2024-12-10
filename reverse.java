4c519daee8f02cad226d2da30445db4accc9d0f1
KrishnaTechie01 <krishna.mathiyalagan01@gmail.com>
2024-12-10 20:06:04 +0530

    Feat : learned to reverse a number

---
+++ reverse.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class reverse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        while (n > 0) {
+            int ld = n % 10;
+            sum = 10 * sum + ld;
+            n = n / 10;
+        }
+
+        System.out.print(sum);
+
+    }
+}
