47ae121e544ff49d52d817bb1701eab7229105fe
KrishnaTechie01 <krishna.mathiyalagan01@gmail.com>
2024-12-10 19:59:48 +0530

    Feat : Learned how to count a digit

---
+++ countdigit.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class countdigit {
+    public static void main(String[] a){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        while (n > 0) {
+            int ld = n % 10;
+            count++;
+            n = n / 10;
+        }
+
+        System.out.print(count);
+
+    }
+    
+}
