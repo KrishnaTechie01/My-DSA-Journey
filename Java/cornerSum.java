aab6b62d5f4c60b8bff3e9e3e32c2477446210c2
KrishnaTechie01 <krishna.mathiyalagan01@gmail.com>
2024-12-13 16:39:09 +0530

    Learned to implement pattern to Sum a fd and ld.

---
+++ Java/cornerSum.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class cornerSum {
+    public static void main(String[] a)
+    {
+        // code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int ld = n % 10;
+        int fd = n / 10;
+        while(fd >= 10){
+            fd = fd / 10;
+        }
+        
+       int sum = ld + fd;
+       System.out.print(sum);
+    }
+}
+
