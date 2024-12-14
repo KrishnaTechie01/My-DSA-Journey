01e7453a68fc8fc843dc8687ed09eaa76ff59e82
KrishnaTechie01 <krishna.mathiyalagan01@gmail.com>
2024-12-14 13:12:43 +0530

    perfect Number

---
+++ Java/perfectNumber.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class perfectNumber {
+    public static void main(String[] a) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 1;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+                if (n / i != i) {
+                    sum = sum + (n / i);
+                }
+            }
+        }
+        if (sum == n) {
+            System.out.print("Perfect Number Of "+n+" is Equal to sum value "+sum);
+        }
+        else {
+            System.out.print("It's Not Perfect Number "+n+" is Not to Equal the sum value "+sum);
+        }
+    }
+}
