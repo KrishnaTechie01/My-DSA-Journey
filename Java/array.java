01ad93a91082e8b610a00082aa92e2a48dcdb267
KrishnaTechie01 <krishna.mathiyalagan01@gmail.com>
2024-12-19 10:00:33 +0530

    iterating in an array and learned to intializing an array

---
+++ Java/array.java
@@ -0,0 +1,9 @@
+class array {
+    public static void main(String[] args) {
+        int[] c = { 93, 63, 66, 79, 93 };
+        for (int i = 0; i < c.length; i++) {
+            System.out.println(c[i]);
+        }
+
+    }
+}
--- Java/countdigit.java
+++ Java/countdigit.java
@@ -6,7 +6,7 @@ public class countdigit {
         int n = sc.nextInt();
         int count = 0;
         while (n > 0) {
-            int ld = n % 10;
+            int id = n % 10;
             count++;
             n = n / 10;
         }
