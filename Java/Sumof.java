0157099c0f44b591da609a38da3fe50446464e87
KrishnaTechie01 <krishna.mathiyalagan01@gmail.com>
2024-12-19 12:55:18 +0530

    Sum of the array values

---
+++ Java/Sumof.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+ class Sumof {
+     public static void main(String[] a) {
+         Scanner sc = new Scanner(System.in);
+         int sum = 0;
+         int n = sc.nextInt();
+         int[] arr = new int[n];
+         for (int i = 0; i < n; i++) {
+             arr[i] = sc.nextInt();
+         }
+         System.out.println("array Values:");
+         for (int i = 0; i < n; i++) {
+             System.out.println(arr[i]);
+             sum += arr[i];
+         }
+         System.out.println("Sum of the array:");
+         System.out.println(sum);
+
+    }
+}
