9bc5ae3cbf1ab90937ac81386dca72cd7f686769
KrishnaTechie01 <krishna.mathiyalagan01@gmail.com>
2025-01-15 21:43:27 +0530

    To count the no of digits

--- Java/countdigit.java
+++ Java/countdigit.java
@@ -6,6 +6,7 @@ public class countdigit {
         int n = sc.nextInt();
         int count = 0;
         while (n > 0) {
+            @SuppressWarnings("unused")
             int id = n % 10;
             count++;
             n = n / 10;
