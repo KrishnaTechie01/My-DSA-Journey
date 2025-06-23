07359d6a13feeacf9a2e2a206593a3be46c50c97
Krishna <krishna.mathiyalagan01@gmail.com>
2025-06-23 10:33:57 +0530

    Delete Java directory

--- Java/array.java
+++
@@ -1,9 +0,0 @@
-class array {
-    public static void main(String[] args) {
-        int[] c = { 93, 63, 66, 79, 93 };
-        for (int i = 0; i < c.length; i++) {
-            System.out.println(c[i]);
-        }
-
-    }
-}
